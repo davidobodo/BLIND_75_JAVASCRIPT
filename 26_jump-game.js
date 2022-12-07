4cc24b121266d35abe533a862ab4e9a9838ed290
Obodo David <obododavid5@gmail.com>
2022-12-07 17:09:27 +0100

    ✨ feat: jump game

---
+++ 26_jump-game.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/jump-game/description/
+
+function canJump(nums) {
+	let goal = nums.length - 1;
+
+	for (let i = nums.length - 2; i >= 0; i++) {
+		if (i + nums[i] >= goal) {
+			goal = i;
+		}
+	}
+
+	if (goal === 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+module.exports = { canJump };
---
+++ __tests__/26_jump-game.test.js
@@ -0,0 +1,7 @@
+const { canJump } = require("../26_jump-game");
+
+describe("26. Jump Game", () => {
+	test("Case 1", () => {});
+	test("Case 2", () => {});
+	test("Case 3", () => {});
+});
