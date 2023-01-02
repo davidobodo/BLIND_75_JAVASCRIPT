c6f7910811662efca76818a13b4eae42a462ad90
Obodo David <obododavid5@gmail.com>
2023-01-02 08:54:47 +0100

    ✨ feat: graph valid tree

--- 33_graph-valid-tree.js
+++ 33_graph-valid-tree.js
@@ -7,8 +7,6 @@ function validTree(n, edges) {
 		adj[i] = [];
 	}
 
-	console.log(adj, "ADJA");
-
 	for (let i = 0; i < edges.length; i++) {
 		const [first, second] = edges[i];
 
@@ -16,9 +14,8 @@ function validTree(n, edges) {
 		adj[second] = [...adj[second], first];
 	}
 
-	console.log(adj, "TEH LIST NOW");
-
 	let visited = new Set();
+
 	function dfs(i, prev) {
 		if (visited.has(i)) {
 			return false;
@@ -26,20 +23,11 @@ function validTree(n, edges) {
 		visited.add(i);
 
 		for (let j = 0; j < adj[i].length; j++) {
-			// if (j === prev) continue;
-
-			console.log(adj[i][j], i, "HERE");
+			if (adj[i][j] === prev) continue;
 			if (!dfs(adj[i][j], i)) {
 				return false;
 			}
 		}
-		// adj[i].forEach((element) => {
-		// 	console.log(element, i, "HERE");
-		// 	// if (element === prev) continue;
-		// 	if (!dfs(element, i)) {
-		// 		return false;
-		// 	}
-		// });
 
 		return true;
 	}
