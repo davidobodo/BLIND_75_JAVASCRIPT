10a484c54b513457ea6222d9430724e07aed873c
Obodo David <obododavid5@gmail.com>
2023-01-09 17:57:56 +0100

    ✨ feat: add test case for two sum

--- 01_two-sum.js
+++ 01_two-sum.js
@@ -1,3 +1,16 @@
+/**
+ *
+ * TIME COMPLEXITY = O(n)
+ * Why?
+ * Because we have only one loop, meaning in worst case scenario we would go through "n" number of elements, which is the number of items in our "nums"
+ */
+
+/**
+ * Space complexity = 0(n)
+ * Why?
+ * Because in the worst case scenario, our "map" used for storage would be filled with "n - 1" (i.e If our compliment is the last number) number of elements
+ */
+
 function twoSum(nums, target) {
 	let map = {};
 
@@ -11,4 +24,4 @@ function twoSum(nums, target) {
 	}
 }
 
-//TODO:
+module.exports = { twoSum };
--- __tests__/01_two-sum.test.js
+++ __tests__/01_two-sum.test.js
@@ -1,1 +1,14 @@
-describe("", () => {});
+const { twoSum } = require("../01_two-sum");
+
+//NOTE: Only added the "sort" to make the tests pass
+describe("01. Two Sum", () => {
+	test("Case 1", () => {
+		expect(twoSum([2, 7, 11, 15], 9).sort()).toEqual([0, 1]);
+	});
+	test("Case 2", () => {
+		expect(twoSum([3, 2, 4], 6).sort()).toEqual([1, 2]);
+	});
+	test("Case 2", () => {
+		expect(twoSum([3, 3], 6).sort()).toEqual([0, 1]);
+	});
+});
