f585afa190c1659d8a1d9c4a93139527287ab144
Obodo David <obododavid5@gmail.com>
2023-01-12 09:56:17 +0100

    ✨ feat: explain contains duplicates

--- 03_contains-duplicates.js
+++ 03_contains-duplicates.js
@@ -1,8 +1,24 @@
 // https://leetcode.com/problems/contains-duplicate/
 
-//-----------------------------------------------------------
-// SOLUTION
-//-----------------------------------------------------------
+/**
+ *
+ * TIME COMPLEXITY = O(n)
+ * Why?
+ * Because in the worst case scenario our loop would run only once through our input elements
+ */
+
+/**
+ * SPACE COMPLEXITY = 0(n)
+ * Why?
+ * Because our space which is our hashmap, could potential have all our input elements
+ */
+
+/**
+ *
+ * EXPLANATION
+ * Hashmap
+ * Iterate through input elements, and check if a key with that value already exists in our hashmap
+ */
 function containsDuplicates(nums) {
 	const map = {};
 
@@ -16,12 +32,5 @@ function containsDuplicates(nums) {
 
 	return false;
 }
-
-//-----------------------------------------------------------
-// EXPLANATION
-//-----------------------------------------------------------
-/**
- * Use a hashmap to track values we have seen, the moment we see a value that already exists then we can say we have found duplicates
- */
 
 module.exports = { containsDuplicates };
