00091b80c3bc68192794cf1a64166e7ad1681aea
Obodo David <obododavid5@gmail.com>
2023-01-12 10:17:39 +0100

    ✨ feat: maximum product subarray explanation

--- 06_maximum-product-subarray.js
+++ 06_maximum-product-subarray.js
@@ -1,8 +1,32 @@
-// https://leetcode.com/problems/maximum-product-subarray/description/
+// https://leetcode.com/problems/maximum-product-subarray
 
-//-----------------------------------------------------------
-// SOLUTION
-//-----------------------------------------------------------
+/**
+ *
+ * TIME COMPLEXITY = O(n)
+ * Why?
+ * Because our three loops would run in O(n)
+ * i.e O(n) + O(n) + O(n) = O(3n)
+ * we ignore the constant, hence our final result becomes O(n)
+ */
+
+/**
+ * SPACE COMPLEXITY = 0(1)
+ * Why?
+ * Because our space which is our 2 variables, would always remain constant irrespective of the amount of numbers in our input
+ */
+
+/**
+ *
+ * EXPLANATION
+ *
+ * Loop through the given array and keep track of both the minimum product and maximum product
+ * We keep track of the minimum because, if you multiple two negative numbers we get a positive number
+ *
+ * On each iteration first analyse what the currMax and currMin would be if our latest number was added to the mix
+ * Then analyze both again if our latest number was made to stand alone without being added to the mix
+ *
+ * After which check for the maximum
+ */
 
 function maxProductSubarray(nums) {
 	let result = nums[0];
@@ -22,18 +46,5 @@ function maxProductSubarray(nums) {
 
 	return result;
 }
-
-//-----------------------------------------------------------
-// EXPLANATION
-//-----------------------------------------------------------
-/**
- * Loop through the given array and keep track of both the minimum product and maximum product
- * We keep track of the minimum because, if you multiple two negative numbers we get a positive number
- *
- * On each iteration first analyse what the currMax and currMin would be if our latest number was added to the mix
- * Then analyze both again is our latest number as made to stand alone without being added to the mix
- *
- * After which check for the maximum
- */
 
 module.exports = { maxProductSubarray };
