ce8fdc9921a6a8c4664f727b31762099ab25af6f
Obodo David <obododavid5@gmail.com>
2023-01-12 10:27:54 +0100

    ✨ feat: explanation and test for find minimum in rotated sorted array

--- 07_find-minimum-in-rotated-sorted-array.js
+++ 07_find-minimum-in-rotated-sorted-array.js
@@ -1,3 +1,30 @@
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array
+
+/**
+ *
+ * TIME COMPLEXITY = O(logn)
+ * Why?
+ * Because on each iteration, the number of elements we are analyze divides by 2
+ */
+
+/**
+ * SPACE COMPLEXITY = 0(1)
+ * Why?
+ * Because our space which is our 2 variables, would always remain constant irrespective of the amount of numbers in our input
+ */
+
+/**
+ *
+ * EXPLANATION
+ * TWO POINTERS
+ *
+ * Initialize two pointers, one at the beginning and one at the end
+ * Get a middle pointer
+ * 	If the number before our middle pointer is greater than what is in our middle pointer, then definitely our middle pointer is our answer since the array is sorted
+ *
+ * We want to go to the smaller side, so add a condition comparing the value at middle pointer with that at both pointers
+ */
+
 function findMin(nums) {
 	let lp = 0;
 	let rp = nums.length - 1;
--- __tests__/07_find-minimum-in-rotated-sorted-array.test.js
+++ __tests__/07_find-minimum-in-rotated-sorted-array.test.js
@@ -1,1 +1,13 @@
 const { findMin } = require("../07_find-minimum-in-rotated-sorted-array");
+
+describe("07. Find Minimum in Rotated Sorted Array", () => {
+	test("Case 1", () => {
+		expect(findMin([3, 4, 5, 1, 2])).toBe(1);
+	});
+	test("Case 2", () => {
+		expect(findMin([4, 5, 6, 7, 0, 1, 2])).toBe(0);
+	});
+	test("Case 3", () => {
+		expect(findMin([11, 13, 15, 17])).toBe(11);
+	});
+});
