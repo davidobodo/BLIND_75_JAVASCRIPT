0f8a192194d2e624c1ea0f8f5f62e814b05f70b6
Obodo David <obododavid5@gmail.com>
2023-01-16 10:37:01 +0100

    ✨ feat: container with most water

--- 10_container-with-most-water.js
+++ 10_container-with-most-water.js
@@ -1,3 +1,27 @@
+// https://leetcode.com/problems/container-with-most-water
+
+/**
+ * TIME COMPLEXITY = O(n)
+ * Why?
+ * Because we would visit all items in our input
+ */
+
+/**
+ * SPACE COMPLEXITY = 0(1)
+ * Why?
+ * Because our space which is our 2 variables, would always remain constant irrespective of the amount of numbers in our input
+ */
+
+/**
+ *
+ * EXPLANATION
+ * Two pointers
+ * Place two pointers at the extreme end
+ * 	find the area
+ * 	move the pointer with the smallest value
+ *
+ */
+
 function maxArea(nums) {
 	let largest = 0;
 	let lp = 0;
