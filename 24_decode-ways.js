0b3e0cdb8b78c366ac947553e357dd8860a7958a
Obodo David <obododavid5@gmail.com>
2023-01-22 10:03:36 +0100

    ✨ feat: explanation for decode ways

--- 24_decode-ways.js
+++ 24_decode-ways.js
@@ -1,3 +1,27 @@
+// https://leetcode.com/problems/house-robber-ii/
+
+/**
+ * TIME COMPLEXITY = O(n)
+ * Why?
+ * Because have a nested whlie loop inside a for loop,
+ */
+
+/**
+ * SPACE COMPLEXITY = 0(n)
+ * Why?
+ * Because our object would store all the corresponding the decode ways
+
+/**
+ *
+ * EXPLANATION
+ * DP - Hashmap DP
+ * 
+ * For this, we solve from back to front using a decrementing for loop
+ * If our string had only one character (i.e the last character then there would only be one way to decode it)
+ * After that we keep decrementing our loop and performing certain checks
+ *
+ */
+
 function decodeWays(s) {
 	let dp = {
 		[s.length]: 1,
