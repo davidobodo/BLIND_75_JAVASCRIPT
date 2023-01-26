3dd262368e1bf129699ffd7a6472e9deb2f8dd25
Obodo David <obododavid5@gmail.com>
2023-01-26 15:55:22 +0100

    ✨ feat: add test for unique paths and explanation for unique paths

--- 25_unique-paths.js
+++ 25_unique-paths.js
@@ -1,12 +1,42 @@
+// https://leetcode.com/problems/unique-paths/
+
+/**
+ * TIME COMPLEXITY = O(n^2)
+ * Why?
+ * Because have a nested whlie loop inside a for loop,
+ */
+
+/**
+ * SPACE COMPLEXITY = 0(n)
+ * Why?
+ * Because our object would store all the corresponding the decode ways
+
+/**
+ *
+ * EXPLANATION
+ * DP - 2 X 2 Matrix
+ * 
+ * Create an m*n matrix
+ * Fill each cell in the matrix with the value 1
+ * Loop through the filled matrix, adding adjacent cells together 
+ *
+ */
+
 function uniquePaths(m, n) {
-	let columns = new Array(n + 1).fill(1, 0, n + 1);
-	let dp = new Array(m + 1).fill(columns, 0, m + 1);
+	let dp = [];
+
+	for (let i = 0; i < m; i++) {
+		const row = new Array(n).fill(1, 0, n);
+		dp[i] = row;
+	}
 
 	for (let i = 1; i < m; i++) {
 		for (let j = 1; j < n; j++) {
-			dp[i + 1][j + 1] = dp[i + 1][j] + dp[i][j + 1];
+			dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
 		}
 	}
 
-	return dp[m][n];
+	return dp[m - 1][n - 1];
 }
+
+module.exports = { uniquePaths };
--- __tests__/25_unique-paths.test.js
+++ __tests__/25_unique-paths.test.js
@@ -1,1 +1,10 @@
-const {} = require("../25_unique-paths");
+const { uniquePaths } = require("../25_unique-paths");
+
+describe("25. Unique Paths", () => {
+	test("Case 1", () => {
+		expect(uniquePaths(3, 7)).toBe(28);
+	});
+	test("Case 2", () => {
+		expect(uniquePaths(3, 2)).toBe(3);
+	});
+});
