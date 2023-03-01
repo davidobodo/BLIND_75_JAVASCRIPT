fe59147d2a6d176d294aef3b3d822473c4bd787c
Obodo David <obododavid5@gmail.com>
2023-03-01 17:47:59 +0100

    ✨feat: longest consecutive sequemce

--- 31_longest-consecutive-sequence.js
+++ 31_longest-consecutive-sequence.js
@@ -1,3 +1,23 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/
+
+/**
+ * TIME COMPLEXITY = O(n^m)
+ * Why?
+ * Because in the worst case scenario the while loop would always run to the last element every single time
+ */
+
+/**
+ * SPACE COMPLEXITY = 0(n)
+ * Why?
+ * Because the size of my set would be the size of the inputs given
+
+/**
+ * create a set since it has a constant lookup time
+ * loop through items in array
+ * 	if the previous value of the current item exists then that value is not the start of a new set
+ * 	if the previous value of the current item doesnt exist, then this is the start of a consecutive set
+ */
+
 function longestConsecutiveSequence(nums) {
 	const mySet = new Set(nums);
 	let maxSequenceCount = 0;
